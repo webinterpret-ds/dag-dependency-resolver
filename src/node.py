0b7d7c2aef5dfe476cd0dc8fb481bc17b6aa370b
patrykjarzyna <patrykjarzyna@gmail.com>
2022-11-13 19:19:08 +0100

    Update typing

--- src/node.py
+++ src/node.py
@@ -13,5 +13,5 @@ from src.enums.node_status import NodeStatus
 @dataclass
 class Node:
     name: Text
-    parents: List[str] = field(default_factory=list)
+    parents: List[Text] = field(default_factory=list)
     status: NodeStatus = field(default=NodeStatus.UNVISITED)
